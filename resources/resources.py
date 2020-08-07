5ba6fa06f78ff56fcd454b272352397fc156c16b
cosikowy <mr.cosik@gmail.com>
2020-08-07 12:47:49 +0200

    api done

--- resources/resources.py
+++ resources/resources.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import pprint
+from datetime import datetime
 from flask_restplus import reqparse
 from flask_restplus import Resource
 
@@ -11,30 +12,65 @@ class Books(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('sorted', location='args')
     parser.add_argument('author', location='args')
-    parser.add_argument('publishedDate', location='args')
-    # parser.add_argument('sorted', location='args')
+    parser.add_argument('published_date', location='args')
 
 
     def get(self, book_id=None):
         args = self.parser.parse_args()
-        print(args)
         
-        # try:
         with open('./db_replacement.json', 'r', encoding='utf-8') as _file:
             db = json.load(_file)
-        # except:
-        #     db = {}
-        #     return 'DB is empty update required'
-        response = {}
+        
+        if book_id:
+            response = self.strip_book_info(db[book_id])
+            return response, 200 
 
-        # pprint.pprint(db['items'][9])
+        db = list(map(lambda x: self.strip_book_info(x), db.values()))
+        response = []
+        if args['author']:
+            for book in db:
+                if isinstance(args['author'], list):
+                    for author in args['author']:
+                        if author in book['authors']:
+                            response.append(book)
+                else:
+                    if args['author'] in book['authors']:
+                        response.append(book)
+        if args['published_date']:
+            for book in db:
+                book_time = datetime.strptime(book['published_date'][:4], '%Y')
+                filter_date = datetime.strptime(args['published_date'][:4], '%Y')
+                if book_time == filter_date:
+                    response.append(book)
+        
+        if len(response)==0:
+            response = db
 
+        if args['sorted']:
+            response.sort(key=lambda book: book['published_date'])
+        elif args['sorted']=='reversed':
+            response.sort(key=lambda book: book['published_date'], reversed=True)
 
+        return response, 200 
 
-        return ('', args), 200, 
+    def strip_book_info(self, book):
+        book_info = {"title":book['volumeInfo']['title'],
+                "authors": book['volumeInfo']['authors'],
+                "published_date":book['volumeInfo']['publishedDate'], 
+                }
+        info = [('categories', 'categories'), ('average_rating', 'averageRating'), ('ratings_count','ratingsCount')]
+        for x in info:
+            try:
+                book_info[x[0]] = book['volumeInfo'][x[1]]
+            except KeyError:
+                book_info[x[0]] = None
 
-    def prepare_response(self, books, **kwargs):
-        pass
+        try:
+            book_info["thumbnail"] = book['volumeInfo']['imageLinks']['thumbnail']
+        except KeyError:
+            book_info["thumbnail"] = None
+
+        return book_info
 
     
 class DBupdate(Resource):
@@ -47,15 +83,16 @@ class DBupdate(Resource):
         q = args['q']
         update = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={q}').json()
         try:
-            with open('./db_replacement.json', 'r') as _file:
+            with open('./db_replacement.json', 'r', encoding='utf-8') as _file:
                 db = json.load(_file)
-        except:
+                print('loaded')
+        except Exception as e:
+            print(e)
             db = {}
         
-        if not db:
-            db = {} 
-        
-        db.update(update)
+        for item in update['items']:
+            for x,y in item.items():
+                db[item['id']] = item
 
         with open('./db_replacement.json', 'w', encoding='utf-8') as _file:
             json.dump(db,_file)
